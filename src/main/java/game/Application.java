8a2b38958b7728c117fba19f447ddf491c3936a2
Lawson Meulman <lawson.meulman@gmail.com>
2019-04-30 16:09:36 +1000

    Added basic logging to application

--- src/main/java/game/Application.java
+++ src/main/java/game/Application.java
@@ -9,10 +9,16 @@ import edu.monash.fit2099.engine.FancyGroundFactory;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Player;
 import edu.monash.fit2099.engine.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Application {
+public class Application{
 
 	public static void main(String[] args) {
+
+		System.setProperty("log4j.configuration","log4j2.xml");
+		Logger logger = LogManager.getLogger(Application.class);
+		logger.info("Starting game");
 		World world = new World(new Display());
 
 		FancyGroundFactory groundFactory = new FancyGroundFactory(new Floor(), new Wall());
@@ -32,15 +38,12 @@ public class Application {
 				".......................");
 		gameMap = new GameMap(groundFactory, map);
 		world.addMap(gameMap);
-		
 		Actor player = new Player("Player", '@', 1, 100);
 		world.addPlayer(player, gameMap, 2, 2);
-		
 		Grunt grunt = new Grunt("Mongo", player);
 		gameMap.addActor(grunt, 0, 0);
 		Grunt grunt2 = new Grunt("Norbert", player);
 		gameMap.addActor(grunt2,  10, 10);
-			
 		world.run();
 	}
 }
