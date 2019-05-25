1846e0ca1f49e51d772b39b9bfc3091a28772851
Lawson Meulman <lawson.meulman@gmail.com>
2019-05-25 15:38:38 +1000

    Boosted goon damage to correct value

---
+++ src/main/java/game/items/ExoSkeleton.java
@@ -0,0 +1,27 @@
+package game.items;
+
+import edu.monash.fit2099.engine.Item;
+import game.status.StatusEffect;
+
+/**
+ * An item that applies a status effect when held by a character.
+ */
+public abstract class StatusItem extends Item {
+
+
+    /**
+     * Build a status item.
+     * @param name The name of the item.
+     * @param displayChar The character to use when the item is dropped.
+     */
+    public StatusItem(String name, char displayChar) {
+        super(name, displayChar);
+    }
+
+    /**
+     * Get the status effect associated with this item.
+     * @return A status effect.
+     */
+    protected abstract StatusEffect getEffect();
+
+}
